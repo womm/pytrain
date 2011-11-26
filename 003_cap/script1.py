991ec3321cfb036d5f989d3468ad7694516c462e
Simone Borselli <womflow@gmail.com>
2011-11-26 21:42:26 +0100

    Esercizi capitolo 3

---
+++ 003_cap/script1.py
@@ -0,0 +1,6 @@
+# A first Python Script (p.42 Learning Python by Lutz)
+import sys			#Load a library module
+print(sys.platform)
+print(2 ** 100)			#Raise 2 to a power
+x = 'Spam!'
+print(x * 8)			#String repetition		
---
+++ 003_cap/threenames.py
@@ -0,0 +1,4 @@
+a = 'dead'			#Define three attributes
+b = 'parrot'			#Exported to other files
+c = 'sketch'
+print(a, b, c)			#also used in this file
